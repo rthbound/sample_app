87c4a042d89d7219b2946214590d95f66344ecdb
Tad Hosford <tad@isotope11.com>
2011-09-16 20:35:26 -0500

    Added Feed, feed still in progress

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,7 @@ before_filter :authenticate, :only => [:create, :destroy]
       flash[:success] = "Micro-posted"
       redirect_to root_path
     else
+      @feed_items = []
       render 'pages/home'
 
     end
@@ -14,11 +15,17 @@ before_filter :authenticate, :only => [:create, :destroy]
 
 
   def destroy
-
+   @micropost.destroy
+   redirect_back_or root_path 
   end
 
   #### not DRY, but code was failing to share with sessions_helper as specified in ApplicationController
   private
+  def authorized_user
+    @micropost = Micropost.find(params[:id])
+    redirect_to root_path unless current_user?(@micropost.user)
+  end
+
   def authenticate
     deny_access unless signed_in?
   end
